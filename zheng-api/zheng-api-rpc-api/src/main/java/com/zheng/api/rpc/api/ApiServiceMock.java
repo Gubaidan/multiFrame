d0e4ec27fc17f64bffd91f4a16f2c6fcfdd5b3d1
night <492407268@qq.com>
2018-04-10 12:00:00 +0800

    修改 ApiServiceMock

--- zheng-api/zheng-api-rpc-api/src/main/java/com/zheng/api/rpc/api/ApiServiceMock.java
+++ zheng-api/zheng-api-rpc-api/src/main/java/com/zheng/api/rpc/api/ApiServiceMock.java
@@ -11,6 +11,9 @@ public class ApiServiceMock implements ApiService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApiServiceMock.class);
 
+
+
+
     @Override
     public String hello(String name) {
         LOGGER.info("ApiServiceMock => hello");
